4434f401358b2c900ef3132305b89e2157f67128
Nirzon Pop <nirzon192@gmail.com>
2021-03-20 16:18:34 +0600

    Custom button

--- lib/ui/home.dart
+++ lib/ui/home.dart
@@ -1,10 +1,10 @@
 import 'package:flutter/material.dart';
 
 class Home extends StatelessWidget {
-
-  _tebEmail(){
+  _tebEmail() {
     debugPrint("Email button Pressed");
   }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -18,21 +18,60 @@ class Home extends StatelessWidget {
           ),
         ),
         actions: <Widget>[
-          IconButton(icon: Icon(Icons.home), onPressed: () => { debugPrint("Home Press")}),
+          IconButton(
+              icon: Icon(Icons.home),
+              onPressed: () => {debugPrint("Home Press")}),
           IconButton(icon: Icon(Icons.email), onPressed: _tebEmail)
         ],
       ),
-      body: Center(
-        child: Text(
-          "Hello world",
-          textDirection: TextDirection.ltr,
-          style: TextStyle(
-              fontSize: 27.5, color: Colors.red[700], fontFamily: 'Play-Bold'),
+      body: Container(
+        alignment: Alignment.center,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            CustomButton(),
+
+          ],
         ),
       ),
       floatingActionButton: FloatingActionButton(
-        child: Text("Click"),
+        child: Icon(Icons.accessible_sharp),
+        onPressed:()=>{
+          debugPrint("Floaing")
+        } ,
       ),
     ));
+  }
+}
+
+/**/
+
+/*body: Center(
+child: Text(
+"Hello world",
+textDirection: TextDirection.ltr,
+style: TextStyle(
+fontSize: 27.5, color: Colors.red[700], fontFamily: 'Play-Bold'),
+),
+),*/
+class CustomButton extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: () {
+        final snackbar = SnackBar(
+          content: Text("Hello again"),
+        );
+        Scaffold.of(context).showSnackBar(snackbar);
+      },
+      child: Container(
+        child: Text("Next page",style: TextStyle(color: Colors.white),),
+        padding: EdgeInsets.all(20.0),
+        decoration: BoxDecoration(
+          color: Colors.deepPurple,
+          borderRadius: BorderRadius.circular(8.0),
+        ),
+      ),
+    );
   }
 }
